fe6d2f0d49427e0b99c1761e86e026e1da85da52
makis <mantzinos@hotmail.com>
2022-11-18 21:15:37 +0200

    more changes

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Calisthenics app best</Text>
+      <Text>Calisthenics app best of the best</Text>
       <StatusBar style='auto' />
     </View>
   );
